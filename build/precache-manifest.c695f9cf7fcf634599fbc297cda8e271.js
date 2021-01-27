813833190550ce82e83d00cff9cf833a9c2d48dc
Luca Battistella <luca.battistella.geo@gmail.com>
2021-01-27 23:44:24 +0100

    Add files via upload

---
+++ build/precache-manifest.c695f9cf7fcf634599fbc297cda8e271.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7c256ae61ae7dcebbac17b59c3a7e17",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8b35cbb1162a838de0dd",
+    "url": "./static/css/2.6b223ed8.chunk.css"
+  },
+  {
+    "revision": "6449cc070fd87f0ec847",
+    "url": "./static/css/main.7b4a2527.chunk.css"
+  },
+  {
+    "revision": "8b35cbb1162a838de0dd",
+    "url": "./static/js/2.54c22014.chunk.js"
+  },
+  {
+    "revision": "e581b3ac89211983c4f9d32746545fbb",
+    "url": "./static/js/2.54c22014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6449cc070fd87f0ec847",
+    "url": "./static/js/main.86562735.chunk.js"
+  },
+  {
+    "revision": "4a90c5928e11ae8d5657",
+    "url": "./static/js/runtime-main.225f5cc0.js"
+  },
+  {
+    "revision": "d688f2ed39d813c2796acf2b397b4f6c",
+    "url": "./static/media/biopama_logo.d688f2ed.svg"
+  }
+]);
