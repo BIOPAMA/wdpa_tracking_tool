492102d0cc323f2b6264fe726f5857bab7688be4
Luca Battistella <luca.battistella.geo@gmail.com>
2022-02-18 15:09:14 +0100

    Add files via upload

---
+++ build/precache-manifest.a8da2c42acbaef2130f6d36267e48dd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e3c3b6b1562d13c1d490f9e98413cf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c49bee6dd5c0259d278",
+    "url": "./static/css/2.6540d719.chunk.css"
+  },
+  {
+    "revision": "d5cc0b1de3db1f442f50",
+    "url": "./static/css/main.5cac097b.chunk.css"
+  },
+  {
+    "revision": "8c49bee6dd5c0259d278",
+    "url": "./static/js/2.5623ff60.chunk.js"
+  },
+  {
+    "revision": "6e2dd25959e7ebcf9f089b909807182d",
+    "url": "./static/js/2.5623ff60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cc0b1de3db1f442f50",
+    "url": "./static/js/main.b8cc012b.chunk.js"
+  },
+  {
+    "revision": "4a90c5928e11ae8d5657",
+    "url": "./static/js/runtime-main.225f5cc0.js"
+  },
+  {
+    "revision": "d688f2ed39d813c2796acf2b397b4f6c",
+    "url": "./static/media/biopama_logo.d688f2ed.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.79cf55ddf7926034d4e8d8d949954e21.js"
+  "./precache-manifest.a8da2c42acbaef2130f6d36267e48dd8.js"
 );
 
 self.addEventListener('message', (event) => {
